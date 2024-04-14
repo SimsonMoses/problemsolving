b705027fc394b56307460cf56dcd6f6e1fd8f14b
simsonmoses <sims91502@gmail.com>
2024-04-14 21:51:33 +0530

    solved: CyclicGraph

---
+++ src/graph/problem/CyclicGraph.java
@@ -0,0 +1,52 @@
+package graph.problem;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Pair{
+    int s,parent;
+    Pair(int s,int parent){
+        this.s = s;
+        this.parent = parent;
+    }
+}
+public class CyclicGraph {
+
+    public static boolean isCyclic(int src, List<List<Integer>> adj, int v) {
+        boolean visited[] = new boolean[v];
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(src,-1));
+        visited[src] = true;
+
+        while(!queue.isEmpty()){
+            Pair pair = queue.poll();
+            int currentElement = pair.s;
+            int parent = pair.parent;
+            for(int i: adj.get(currentElement)){
+                if(!visited[i]){
+                    visited[i] = true;
+                    queue.add(new Pair(i,currentElement));
+                } else if (parent!=i) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        list.add(0,List.of());
+        list.add(1, List.of(2, 3));
+        list.add(2, List.of(1, 4));
+        list.add(3, List.of(1,6,5));
+        list.add(4, List.of(2));
+        list.add(5, List.of(3,7));
+        list.add(6, List.of(3));
+        list.add(7, List.of(5));
+        System.out.println(isCyclic(1,list, 8));
+
+    }
+}
